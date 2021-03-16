c797ae6c0964371c1d26d0c08870f6788d0e071d
조정현 <76645095+jojaeng2@users.noreply.github.com>
2021-03-16 13:54:56 +0900

    Add Copy Function

--- public/js/text.js
+++ public/js/text.js
@@ -1,6 +1,17 @@
-const color_child = document.querySelector("color_child");
+const color_child = Array.from(document.querySelectorAll('.color_child')),
+        color_value = document.querySelectorAll('.background_color');
 
-function init(){
-    alert('a');   
+color_child.forEach(item => {
+    item.addEventListener("click",(e)=>{
+        Copycolor();
+    })
+});
+
+function Copycolor(){
+    var clipboard = new ClipboardJS('.color_child');
+    clipboard.on('success',(e)=>{
+        console.log(e);
+    })
+    clipboard.on('error',(e)=>{
+    })
 }
-init();
